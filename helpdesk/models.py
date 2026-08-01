16cc662f2ed282c84e0d65a893ee341c4c0ac5c1
Rodney <83302930+rave-net@users.noreply.github.com>
2026-08-01 12:53:49 -0500

    Update models for Django 5.2

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -1,36 +1,40 @@
-
-
 from django.db import models
-from django.core.exceptions import ValidationError
 
-# Create your models here.
+
 class GWSettings(models.Model):
-    #id = models.PositiveSmallIntegerField(primary_key=True, unique=True)
     gwHost = models.CharField(max_length=120)
     gwPort = models.PositiveSmallIntegerField()
     gwAdmin = models.CharField(max_length=64)
-    gwPass = models.CharField(max_length=64)
+    gwPass = models.CharField(max_length=256)
 
+    class Meta:
+        verbose_name_plural = "GroupWise settings"
+
+    def __str__(self):
+        return f"{self.gwHost}:{self.gwPort}"
 
 
 class Admin(models.Model):
-    REQUIRED_FIELDS = ('username','role')
-    ADMIN = 'AD'
-    HELPDESK = 'HD'
-    HELPDESKLITE = 'HL'
-    PASSWORD = 'PW'
-    ADDRBOOK = 'AB'
+    ADMIN = "AD"
+    HELPDESK = "HD"
+    HELPDESKLITE = "HL"
+    PASSWORD = "PW"
+    ADDRBOOK = "AB"
+
     roleChoices = (
-        (ADMIN, 'Administrator'),
-        (HELPDESK, 'Helpdesk'),
-        (HELPDESKLITE, 'Helpdesk Lite'),
-        (ADDRBOOK, 'Address Book'),
-        (PASSWORD, 'Password'),
+        (ADMIN, "Administrator"),
+        (HELPDESK, "Helpdesk"),
+        (HELPDESKLITE, "Helpdesk Lite"),
+        (ADDRBOOK, "Address Book"),
+        (PASSWORD, "Password"),
     )
-    #USERNAME_FIELD = models.CharField(max_length=64)
+
     username = models.CharField(max_length=128, null=True)
     first_name = models.CharField(max_length=64, null=True, blank=True)
     last_name = models.CharField(max_length=64, null=True, blank=True)
     password = models.CharField(max_length=256, null=True)
     password2 = models.CharField(max_length=256, null=True, blank=True)
     role = models.CharField(max_length=4, choices=roleChoices, default=HELPDESK)
+
+    def __str__(self):
+        return self.username or f"Admin {self.pk}"
